crdroidandroid/android_packages_apps_crDroidSettings

crdroid: Add FOD icon picker and wallpaper based color option [2/2]
Co-authored-by: Stallix <joey@evolution-x.org>
Co-authored-by: rohanpurohit <purohit.rohan@gmail.com>

src/com/crdroid/settings/fragments/lockscreen/FODIconPickerFragment.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2020 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.lockscreen;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import com.android.settings.R;
+
+import android.content.res.Resources;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class FODIconPickerFragment extends SettingsPreferenceFragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.fod_picker);
+
+        getActivity().getActionBar().setTitle(R.string.fod_icon_picker_title);
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.fod_icon_picker_footer);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+    }
+}

Team-Xtreme/packages_apps_freedomhub

src/com/aim/freedomhub/fragments/FODIconPickerFragment.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2020 AIMROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aim.freedomhub.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import com.android.settings.R;
+
+import android.content.res.Resources;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class FODIconPickerFragment extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.fod_picker);
+
+        getActivity().getActionBar().setTitle(R.string.fod_icon_picker_title);
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.fod_icon_picker_footer);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.FREEDOMHUB;
+    }
+}