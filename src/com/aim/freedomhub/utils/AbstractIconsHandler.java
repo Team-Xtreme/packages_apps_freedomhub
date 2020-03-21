fa9c0c6500ccd653cc3c995cf067d6a3c37e1583
Rpingat <pingatravi@gmail.com>
2020-03-21 13:29:38 +0530

    Revert "WIP"
    This reverts commit 08045336334c6afcc0e53c7887b1e1bdbd30e0f8.

--- src/com/aim/freedomhub/utils/AbstractIconsHandler.java
+++
@@ -1,9 +0,0 @@
-package com.aim.freedomhub.utils;
-
-import android.content.Context;
-import android.content.pm.ActivityInfo;
-import android.graphics.drawable.Drawable;
-
-public interface AbstractIconsHandler {
-    public Drawable getIconFromHandler(Context context, ActivityInfo info);
-}
